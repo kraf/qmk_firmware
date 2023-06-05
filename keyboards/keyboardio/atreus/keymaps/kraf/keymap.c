f28b8f8d8d12bc919e74a9c83a2ce086eff46412
Filip Krasnianski <filip@gomore.com>
2023-06-05 19:27:04 +0200

    V1 of symbols

--- keyboards/keyboardio/atreus/keymaps/kraf/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/kraf/keymap.c
@@ -17,24 +17,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,                                                    KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,
   KC_A,              LGUI_T(KC_S),      LALT_T(KC_D),      LCTL_T(KC_F),      KC_G,                                                    KC_H,              RCTL_T(KC_J),      LALT_T(KC_K),      RGUI_T(KC_L),      KC_QUOT,
   LSFT_T(KC_Z),      ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_GRV,            KC_BSLS,           KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    RSFT_T(KC_SLSH),
-  KC_ESC,            KC_TAB,            KC_LGUI,           KC_ESC,            LT(U_NAV,KC_BSPC), KC_TAB,            LT(U_SYM,KC_ENT),  LT(U_NUM,KC_SPC),  KC_RSFT,           KC_MINS,           KC_SCLN,           KC_ENT ),
+  KC_ESC,            KC_TAB,            KC_LGUI,           KC_ESC,            LT(U_NAV,KC_BSPC), KC_TAB,            LT(U_SYM,KC_ENT),  LT(U_NUM,KC_SPC),  KC_DEL,            KC_MINS,           KC_SCLN,           KC_ENT ),
 
   [U_NAV] = LAYOUT( /* MIRYOKU_ALTERNATIVES_NAV_INVERTEDT */
-  KC_NO,             KC_NO,             KC_NO,             DF(U_BASE),        KC_NO,                                                   KC_INS,            KC_HOME,           KC_UP,             KC_END,            KC_PGUP,
-  KC_NO,             KC_LGUI,           KC_LALT,           KC_LCTL,           KC_NO,                                                   KC_CAPS,           KC_LEFT,           KC_DOWN,           KC_RGHT,           KC_PGDN,
-  KC_LSFT,           KC_ALGR,           DF(U_NUM),         DF(U_NAV),         KC_NO,             KC_GRV,            KC_BSLS,           KC_LBRC,           KC_LCRC,           KC_NO,             KC_NO,             KC_NO,
-  KC_NO,             KC_NO,             KC_LGUI,           KC_NO,             KC_NO,             KC_NO,             KC_ENT,            KC_SPC,            KC_RSFT,           KC_MINS,           KC_NO,             KC_NO),
+  KC_NO,             KC_NO,             KC_NO,             DF(U_BASE),        KC_NO,                                                   KC_PGUP,           KC_HOME,           KC_UP,             KC_END,            KC_CAPS,
+  KC_NO,             KC_LGUI,           KC_LALT,           KC_LCTL,           KC_NO,                                                   KC_PGDN,           KC_LEFT,           KC_DOWN,           KC_RGHT,           KC_INS,
+  KC_LSFT,           KC_ALGR,           DF(U_NUM),         DF(U_NAV),         KC_NO,             KC_GRV,            KC_BSLS,           KC_VOLD,           KC_VOLU,           KC_BRID,           KC_BRIU,           KC_MUTE,
+  KC_NO,             KC_NO,             KC_LGUI,           KC_NO,             KC_NO,             KC_TAB,            KC_ENT,            KC_SPC,            KC_DEL,            KC_MINS,           KC_NO,             KC_NO),
 
   [U_NUM] = LAYOUT( /* MIRYOKU_ALTERNATIVES_NUM */
   /* KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,           KC_NO,             DF(U_BASE),        DF(U_EXTRA),       DF(U_TAP),         RESET, */
-  KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,                                                 KC_NO,             KC_NO,             KC_NO,             KC_NO,             KC_NO,
-  KC_SCLN,           KC_4,              KC_5,              KC_6,              KC_EQL,                                                  KC_NO,             KC_LCTL,           KC_LALT,           KC_LGUI,           KC_NO,
-  KC_GRV,            KC_1,              KC_2,              KC_3,              KC_BSLS,           KC_GRV,            KC_BSLS,           KC_NO,             KC_NO,             KC_NO,             KC_ALGR,           KC_LSFT,
-  KC_NO,             KC_NO,             KC_NO,             KC_DOT,            KC_0,              KC_MINS,           KC_NO,             KC_NO,             KC_RSFT,           KC_NO,             KC_NO,             KC_NO),
+  KC_GRV,            KC_7,              KC_8,              KC_9,              KC_EQL,                                                  KC_NO,             KC_NO,             KC_NO,             KC_NO,             KC_NO,
+  KC_LBRC,           KC_4,              KC_5,              KC_6,              KC_RBRC,                                                 KC_NO,             KC_LCTL,           KC_LALT,           KC_LGUI,           KC_NO,
+  KC_BSLS,           KC_1,              KC_2,              KC_3,              KC_SCLN,           KC_GRV,            KC_BSLS,           KC_NO,             KC_NO,             KC_NO,             KC_ALGR,           KC_LSFT,
+  KC_NO,             KC_NO,             KC_NO,             KC_DOT,            KC_0,              KC_MINS,           KC_NO,             KC_NO,             KC_DEL,            KC_NO,             KC_NO,             KC_NO),
 
   [U_SYM] = LAYOUT( /* MIRYOKU_ALTERNATIVES_SYM */
-  KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_RCBR,                                                 KC_NO,             KC_NO,             KC_NO,             KC_NO,             KC_NO,
-  KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,                                                 KC_NO,             KC_LCTL,           KC_LALT,           KC_LGUI,           KC_NO,
-  KC_TILD,           KC_EXLM,           KC_AT,             KC_HASH,           KC_PIPE,           KC_GRV,            KC_BSLS,           KC_NO,             KC_NO,             KC_NO,             KC_ALGR,           KC_LSFT,
-  KC_NO,             KC_NO,             KC_NO,             KC_LPRN,           KC_RPRN,           KC_UNDS,           KC_NO,             KC_NO,             KC_RSFT,           KC_NO,             KC_NO,             KC_NO)
+  KC_TILD,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_PLUS,                                                 KC_NO,             KC_NO,             KC_NO,             KC_NO,             KC_NO,
+  KC_LCBR,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_RCBR,                                                 KC_NO,             KC_LCTL,           KC_LALT,           KC_LGUI,           KC_NO,
+  KC_PIPE,           KC_EXLM,           KC_AT,             KC_HASH,           KC_COLN,           KC_GRV,            KC_BSLS,           KC_NO,             KC_NO,             KC_NO,             KC_ALGR,           KC_LSFT,
+  KC_NO,             KC_NO,             KC_NO,             KC_LPRN,           KC_RPRN,           KC_UNDS,           KC_NO,             KC_NO,             KC_DEL,            KC_NO,             KC_NO,             KC_NO)
 };
